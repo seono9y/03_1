afb3fc35f88dac504b18a9d42aa56becd519437d
seono9y <144989211+seono9y@users.noreply.github.com>
2023-09-24 13:22:11 +0900

    04

--- exercise_03/main.c
+++ exercise_03/main.c
@@ -7,16 +7,18 @@
 
 #include <stdio.h>
 
-int main(void)
-{
-    char c;
-    int i;
+int main(int argc, char *argv[]) {
+    int a;
+    int b;
     
-    printf("input a number :");
-    scanf("%c", &c);
+    printf("분자를 입력하세요 : ");
+    scanf("%d",&a);
     
-    i=c-'0';
-    printf("The input number is %i\n",i);
+    printf("분모를 입력하세요 : ");
+    scanf("%d",&b);
+    
+    printf("나누기의 결과는 %0.6f 입니다.",(float)a/b);
     
     return 0;
+    
 }
